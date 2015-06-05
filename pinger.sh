81b644b99b593e6163c7a643afc9d15e66015aae
Linus Wallgren <linus.wallgren@scypho.com>
2015-06-05 17:45:34 +0200

    Initial atempt at a kafka pinger

---
+++ pinger.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -u
+
+BROKERS="$1"
+TOPIC="$2"
+
+fping -A -e -l -b12 -p 60000 -g 10.0.0.0/22 2>/dev/null | while read -r PING_RESPONSE; do
+    ip_regex='[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}'
+    regex="($ip_regex)[ ]+: \[(.*)\], (.*) bytes, (.*) ms \((.*) avg, (.*)% loss\)" 
+    [[ $PING_RESPONSE =~ $regex ]]
+    IP="${BASH_REMATCH[1]}"
+    COUNT="${BASH_REMATCH[2]}"
+    SIZE="${BASH_REMATCH[3]}"
+    ROUNDTRIP="${BASH_REMATCH[4]}"
+    AVG="${BASH_REMATCH[5]}"
+    LOSS_PERCENT="${BASH_REMATCH[6]}"
+
+    NOW=$(date --iso-8601=ns)
+
+    jq --unbuffered \
+        --null-input \
+        --compact-output --ascii-output --monochrome-output \
+        --arg ip "$IP" \
+        --arg size "$SIZE" \
+        --arg roundtrip "$ROUNDTRIP" \
+        --arg now "$NOW" \
+        '{ "ip": $ip, "size": $size, "roundtrip": $roundtrip, "time": $now }'
+
+
+done | kafkacat -P -b $BROKERS -t "$TOPIC"
